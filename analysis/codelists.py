08c60b76c2c5389f00d1b8634388eb2fea66cecb
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2022-08-11 10:31:49 +0100

    Covariates
    List of covariates, code lists.

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -53,7 +53,206 @@ smoking_unclear = codelist_from_csv(
     category_column="Category",
 )
 
+# Dementia
+dementia_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-dementia_snomed.csv",
+    system="snomed",
+    column="code",
+)
+dementia_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-dementia_icd10.csv",
+    system="icd10",
+    column="code",
+)
 
+# Dementia vascular 
+dementia_vascular_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-dementia_vascular_snomed.csv",
+    system="snomed",
+    column="code",
+)
+dementia_vascular_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-dementia_vascular_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# Liver disease
+liver_disease_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-liver_disease_snomed.csv",
+    system="snomed",
+    column="code",
+)
+liver_disease_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-liver_disease_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# Chronic Kidney disease
+ckd_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-ckd_snomed.csv",
+    system="snomed",
+    column="code",
+)
+ckd_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-ckd_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# Cancer
+cancer_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-cancer_snomed.csv",
+    system="snomed",
+    column="code",
+)
+cancer_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-cancer_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# HYpertension
+hypertension_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-hypertension_icd10.csv",
+    system="icd10",
+    column="code",
+)
+hypertension_drugs_dmd = codelist_from_csv(
+    "codelists/user-elsie_horne-hypertension_drugs_dmd.csv",
+    system="snomed",
+    column="dmd_id",
+)
+hypertension_snomed_clinical = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-hyp_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+# Diabetes
+# Type 1 diabetes
+diabetes_type1_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-type-1-diabetes.csv",
+    system="ctv3",
+    column="code",
+)
+# Type 1 diabetes secondary care
+diabetes_type1_icd10 = codelist_from_csv(
+    "codelists/opensafely-type-1-diabetes-secondary-care.csv",
+    system="icd10",
+    column="icd10_code",
+)
+# Type 2 diabetes
+diabetes_type2_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-type-2-diabetes.csv",
+    system="ctv3",
+    column="code",
+)
+# Type 2 diabetes secondary care
+diabetes_type2_icd10 = codelist_from_csv(
+    "codelists/user-r_denholm-type-2-diabetes-secondary-care-bristol.csv",
+    system="icd10",
+    column="code",
+)
+# Non-diagnostic diabetes codes
+diabetes_diagnostic_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-nondiagnostic-diabetes-codes.csv",
+    system="ctv3",
+    column="code",
+)
+# Other or non-specific diabetes
+diabetes_other_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-other-or-nonspecific-diabetes.csv",
+    system="ctv3",
+    column="code",
+)
+# Gestational diabetes
+diabetes_gestational_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-gestational-diabetes.csv",
+    system="ctv3",
+    column="code",
+)
+# Insulin medication 
+insulin_snomed_clinical = codelist_from_csv(
+     "codelists/opensafely-insulin-medication.csv",
+     system="snomed",
+     column="id",
+)
+# Antidiabetic drugs
+antidiabetic_drugs_snomed_clinical = codelist_from_csv(
+     "codelists/opensafely-antidiabetic-drugs.csv",
+     system="snomed",
+     column="id",
+)
+# Antidiabetic drugs - non metformin
+non_metformin_dmd = codelist_from_csv(
+    "codelists/user-r_denholm-non-metformin-antidiabetic-drugs_bristol.csv", 
+    system="snomed", 
+    column="id",
+)
+# Prediabetes
+prediabetes_snomed = codelist_from_csv(
+    "codelists/opensafely-prediabetes-snomed.csv",
+    system="snomed",
+    column="code",
+)
+
+# COPD
+copd_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-copd_snomed.csv",
+    system="snomed",
+    column="code",
+)
+copd_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-copd_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# AMI
+ami_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-ami_snomed.csv",
+    system="snomed",
+    column="code",
+)
+ami_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-ami_icd10.csv",
+    system="icd10",
+    column="code",
+)
+ami_prior_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-ami_prior_icd10.csv",
+    system="icd10",
+    column="code",
+)
+artery_dissect_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-artery_dissect_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# Stroke 
+stroke_isch_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_isch_icd10.csv",
+    system="icd10",
+    column="code",
+)
+stroke_isch_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-stroke_isch_snomed.csv",
+    system="snomed",
+    column="code",
+)
+stroke_sah_hs_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_sah_hs_icd10.csv",
+    system="icd10",
+    column="code",
+)
+stroke_sah_hs_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-stroke_sah_hs_snomed.csv",
+    system="snomed",
+    column="code",
+)
 
 # BMI
 bmi_obesity_snomed_clinical = codelist_from_csv(
