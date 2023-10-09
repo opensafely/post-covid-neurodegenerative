c06dc68871d7b52aa2f51345ddd348cb496d001b
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2023-10-09 13:18:47 +0100

    Venn diagram update
    Column names fixed in common_variables script. Venn diagram only adapts the any_dementia outcome.

--- analysis/descriptives/venn.R
+++ analysis/descriptives/venn.R
@@ -40,12 +40,6 @@ print('Load Venn data')
 
 venn <- readr::read_rds(paste0("output/venn_",cohort,".rds"))
 
-# rename columns
-venn <- venn %>%
-  rename_at(vars(matches(c("_symptoms"))), ~ str_remove(., c("_symptoms"))) %>%
-  rename_with(~ str_replace(., "tmp_out_date_alzheimer_", "tmp_out_date_alzheimer_disease_")) %>%
-  rename_with(~ str_replace(., "tmp_out_date_parkinson_", "tmp_out_date_parkinson_disease_"))
-
 # Create empty output table ----------------------------------------------------
 print('Create empty output table')
 
