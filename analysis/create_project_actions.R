6b7170df9bf6e6064e9d1df34cbbf4708f1f4eae
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2024-01-11 16:21:25 +0000

    Fixed create project actions
    Added missing file to stage1_data_cleaning function.

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -148,6 +148,9 @@ stage1_data_cleaning <- function(cohort){
       moderately_sensitive = list(
         consort = glue("output/consort_{cohort}.csv"),
         consort_rounded = glue("output/consort_{cohort}_rounded.csv")
+      ),
+      highly_sensitive = list(
+        cohort = glue("output/input_{cohort}_stage1.rds")
       )
     ),
     action(
@@ -504,16 +507,16 @@ actions_list <- splice(
   
   ## Model output --------------------------------------------------------------
   
-  comment("Stage 6 - make model output"),
-
-  action(
-    name = "make_model_output",
-    run = "r:latest analysis/model/make_model_output.R",
-    needs = as.list(paste0("cox_ipw-",success$name)),
-    moderately_sensitive = list(
-      model_output = glue("output/model_output.csv")
-    )
-  ),
+  # comment("Stage 6 - make model output"),
+  # 
+  # action(
+  #   name = "make_model_output",
+  #   run = "r:latest analysis/model/make_model_output.R",
+  #   needs = as.list(paste0("cox_ipw-",success$name)),
+  #   moderately_sensitive = list(
+  #     model_output = glue("output/model_output.csv")
+  #   )
+  # ),
   
   ## AER table -----------------------------------------------------------------
   
