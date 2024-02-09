cd915dc7833d05c67ee9edb302ee50e3606178ad
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2024-02-09 14:19:21 +0000

    Changed total events column
    Total events columns recalculated and columns order changed (according to dummy table 2).

--- analysis/descriptives/table2.R
+++ analysis/descriptives/table2.R
@@ -125,13 +125,29 @@ table2[,setdiff(colnames(table2),c("name","cohort","exposure","outcome","analysi
                                                                                                 FUN=function(y){roundmid_any(as.numeric(y), to=threshold)})
 
 # Rename columns (output redaction) --------------------------------------------
+print("Rename columns for output redaction")
 
-names(df)[names(df) == "unexposed_events"] <- 'unexposed_events_midpoint6'
-names(df)[names(df) == "exposed_events"] <- "exposed_events_midpoint6"
-names(df)[names(df) == "total_events"] <- "total_events_midpoint6_derived"
-names(df)[names(df) == "day0_events"] <- "day0_events_midpoint6"
-names(df)[names(df) == "total_exposed"] <- "total_exposed_midpoint6"
-names(df)[names(df) == "sample_size"] <- "sample_size_midpoint6"
+names(table2)[names(table2) == "unexposed_events"] <- 'unexposed_events_midpoint6'
+names(table2)[names(table2) == "exposed_events"] <- "exposed_events_midpoint6"
+names(table2)[names(table2) == "day0_events"] <- "day0_events_midpoint6"
+names(table2)[names(table2) == "total_exposed"] <- "total_exposed_midpoint6"
+names(table2)[names(table2) == "sample_size"] <- "sample_size_midpoint6"
+
+# Recalculate total events midpoint --------------------------------------------
+print("Recalculate total events (midpoint6 derived) column")
+
+table2$total_events_midpoint_derived <- table2$unexposed_events_midpoint6 + table2$exposed_events_midpoint6
+
+# Remove total events column ---------------------------------------------------
+print("Remove total events column")
+
+table2$total_events <- NULL
+
+# Relocate columns -------------------------------------------------------------
+print("Relocate columns following dummy table 2")
+
+table2 <- table2 %>%
+  relocate(total_events_midpoint_derived, .after = total_person_days)
 
 # Save Table 2 -----------------------------------------------------------------
 print('Save rounded Table 2')
