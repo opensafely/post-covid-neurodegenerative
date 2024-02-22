8e4d4aec490a98f015c7847a841d3ff790af8a0b
José Ignacio Cuitún Coronado <joseignacio.cuituncoronado@bristol.ac.uk>
2024-02-22 15:33:10 +0000

    Missing line
    Added missing part in the end_vax_exposure (deregistration_date)

--- analysis/prelim.R
+++ analysis/prelim.R
@@ -25,7 +25,7 @@ prelim_data <- prelim_data %>%
   mutate(index_vax = max(c(vax_date_covid_2_offset, delta_date), na.rm=T),
          index_unvax =  max(c(vax_date_eligible_offset, delta_date), na.rm=T),
          end_vax = min(c(death_date, delta_end_date, deregistration_date), na.rm=T),
-         end_unvax_exposure = min(c(death_date, delta_end_date, vax_date_covid_1), na.rm=T),
+         end_unvax_exposure = min(c(death_date, delta_end_date, vax_date_covid_1, deregistration_date), na.rm=T),
          end_unvax_outcome = min(c(death_date, delta_end_date, deregistration_date, deregistration_date), na.rm=T), 
          end_prevax_exposure = min(c(vax_date_eligible, death_date, vax_date_covid_1, all_eligible_date, deregistration_date), na.rm=T),
          end_prevax_outcome = min(c(death_date, delta_end_date, deregistration_date), na.rm=T))
