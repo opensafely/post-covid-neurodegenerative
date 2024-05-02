85e0c418119f6cc3dee42c9d90fa8730c3123cd4
José Ignacio Cuitún Coronado <joseignacio.cuituncoronado@bristol.ac.uk>
2024-05-02 12:14:39 +0100

    Rename columns table1
    Removed "midpoint6" and "midpoint6 derived" from column names.

--- analysis/post_release/manuscript_table1.R
+++ analysis/post_release/manuscript_table1.R
@@ -4,20 +4,27 @@ print("Load data")
 df <- readr::read_csv(path_table1,
                       show_col_types = FALSE)
 
+# Rename columns for Results pack ----------------------------------------------
+print("Rename columns")
+
+df <- dplyr::rename(df,
+                    "N (%)" = "N (%) midpoint6 derived",
+                    "COVID-19 diagnoses" = "COVID-19 diagnoses midpoint6")
+
 # Pivot table ------------------------------------------------------------------
 print("Pivot table")
 
 df <- tidyr::pivot_wider(df, 
                          names_from = "cohort",
-                         values_from = c("N (%) midpoint6 derived","COVID-19 diagnoses midpoint6"))
+                         values_from = c("N (%)","COVID-19 diagnoses"))
 
 # Tidy table -------------------------------------------------------------------
 print("Tidy table")
 
 df <- df[,c("Characteristic","Subcharacteristic",
-            paste0(c("N (%) midpoint6 derived","COVID-19 diagnoses midpoint6"),"_prevax"),
-            paste0(c("N (%) midpoint6 derived","COVID-19 diagnoses midpoint6"),"_vax"),
-            paste0(c("N (%) midpoint6 derived","COVID-19 diagnoses midpoint6"),"_unvax"))]
+            paste0(c("N (%)","COVID-19 diagnoses"),"_prevax"),
+            paste0(c("N (%)","COVID-19 diagnoses"),"_vax"),
+            paste0(c("N (%)","COVID-19 diagnoses"),"_unvax"))]
 
 # Save table -------------------------------------------------------------------
 print("Save table")
