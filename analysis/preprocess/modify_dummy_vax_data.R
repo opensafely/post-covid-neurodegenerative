ca94d3202264904ea03790f64fffce85f7e68953
Harry T Mason <harry.mason@outlook.com>
2025-01-30 14:28:11 +0000

    Clean modify_dummy_vax_data
    Literally just some empty space cleaning, no actual edits

--- analysis/preprocess/modify_dummy_vax_data.R
+++ analysis/preprocess/modify_dummy_vax_data.R
@@ -4,7 +4,7 @@
 # Set seed ---------------------------------------------------------------------
 set.seed(1)
 
-# Change first jab date so that they have roughly correct distribution  
+# Change first jab date so that they have roughly correct distribution
 df <- df %>%
   mutate(
     vax_date_Pfizer_1 = as.Date(vax_date_eligible) + days(round(rnorm(nrow(.), mean = 10, sd = 3))),
@@ -43,21 +43,21 @@ df <- df %>%
                   vaccine_1_type %in% "Moderna",
                   .x,
                   NA_Date_))) %>%
-  
+
   mutate(across(matches("vax_date\\w+_1"),
                 ~ if_else(
                   vaccine_1_type %in% "None",
                   NA_Date_,
                   .x
                 ))) %>%
-  
+
   #Change date for the second jab
   mutate(
     vax_date_Pfizer_2 = vax_date_Pfizer_1 + days(round(rnorm(nrow(.), mean = 10*7, sd = 3))),
     vax_date_AstraZeneca_2 = vax_date_AstraZeneca_1 + days(round(rnorm(nrow(.), mean = 10*7, sd = 3))),
     vax_date_Moderna_2 = vax_date_Moderna_1  + days(round(rnorm(nrow(.), mean = 10*7, sd = 3))),
   ) %>%
-  
+
   # Set 2nd vaccine type
   mutate(vaccine_2_type =  ifelse(runif(nrow(df),0,1)>0.95 & vaccine_1_type!="None",
                                   sample(
@@ -68,9 +68,9 @@ df <- df %>%
                                   ),
                                   vaccine_1_type)
   ) %>%
-  
-  #Set second jab date according to type and set others to NA 
-  
+
+  #Set second jab date according to type and set others to NA
+
   mutate(across(vax_date_Pfizer_2,
                 ~if_else(
                   vaccine_2_type %in% "Pfizer",
@@ -86,14 +86,14 @@ df <- df %>%
                   vaccine_1_type %in% "Moderna",
                   .x,
                   NA_Date_))) %>%
-  
+
   mutate(across(matches("vax_date\\w+_2"),
                 ~ if_else(
                   vaccine_2_type %in% "None",
                   NA_Date_,
                   .x
                 ))) %>%
-  
+
   # Set to NA if jab is missing
   mutate(across(vax_date_Pfizer_2,
                 ~if_else(
@@ -110,7 +110,7 @@ df <- df %>%
                   missing_moderna_2,
                   NA_Date_,
                   .x))) %>%
-  
+
   #Set 3rd jab type
   mutate(vaccine_3_type =  ifelse( vaccine_2_type!="None",
                                    sample(
@@ -119,18 +119,18 @@ df <- df %>%
                                      replace = TRUE,
                                      prob = c(0.6, 0.1, 0.3, 0.1)
                                    ),vaccine_2_type
-  )
+                                 )
   ) %>%
-  
+
   #Change 3rd jab date
   mutate(
     vax_date_Pfizer_3 = vax_date_Pfizer_2 + days(round(rnorm(nrow(.), mean = 6*4*7, sd = 7))),
     vax_date_AstraZeneca_3 = vax_date_AstraZeneca_2 + days(round(rnorm(nrow(.), mean = 6*4*7, sd = 7))),
     vax_date_Moderna_3 = vax_date_Moderna_2 + days(round(rnorm(nrow(.), mean = 6*4*7, sd = 7))),
   ) %>%
-  
-  #Set 3rd jab date according to type and set others to NA 
-  
+
+  #Set 3rd jab date according to type and set others to NA
+
   mutate(across(vax_date_Pfizer_3,
                 ~if_else(
                   vaccine_3_type %in% "Pfizer",
@@ -146,7 +146,7 @@ df <- df %>%
                   vaccine_1_type %in% "Moderna",
                   .x,
                   NA_Date_))) %>%
-  
+
   mutate(across(matches("vax_date\\w+_3"),
                 ~ if_else(
                   vaccine_3_type %in% "None",
@@ -169,5 +169,5 @@ df <- df %>%
                   missing_moderna_3,
                   NA_Date_,
                   .x)))%>%
-  
+
   select(-starts_with("missing"),-matches("vaccine_\\d_type"))
