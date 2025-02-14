11038be18484a6e2552a7268f136803cdf0119b5
Harry T Mason <harry.mason@outlook.com>
2025-02-14 13:05:38 +0000

    Strat_ variable
    Add criteria for strat_cat_region variable inclusion

--- analysis/preprocess/preprocess_data.R
+++ analysis/preprocess/preprocess_data.R
@@ -96,16 +96,17 @@ df1 <- df %>%
   select(patient_id,
          starts_with("index_date"),
          starts_with("end_date_"),
-         contains("sub_"), # Subgroups
-         contains("exp_"), # Exposures
-         contains("out_"), # Outcomes
-         contains("cov_"), # Covariates
-         contains("inex_"), # Inclusion/exclusion
-         contains("cens_"), # Censor
-         contains("qa_"), # Quality assurance
+         contains("sub_"),   # Subgroups
+         contains("exp_"),   # Exposures
+         contains("out_"),   # Outcomes
+         contains("cov_"),   # Covariates
+         contains("strat_"), # Strata
+         contains("inex_"),  # Inclusion/exclusion
+         contains("cens_"),  # Censor
+         contains("qa_"),    # Quality assurance
          contains("vax_date_eligible"), # Vaccination eligibility
-         contains("vax_date_"), # Vaccination dates and vax type 
-         contains("vax_cat_") # Vaccination products
+         contains("vax_date_"),         # Vaccination dates and vax type
+         contains("vax_cat_")           # Vaccination products
   )
 
 df1[,colnames(df)[grepl("tmp_",colnames(df))]] <- NULL
