47ceed76cd7b475c6257c907b1a92ea0a1e7f622
Harry T Mason <harry.mason@outlook.com>
2025-02-20 16:27:41 +0000

    dataset_definition output updates
    reindexing the inputs into dataset_definitions, as they now come from output/dataset_definitions/ (rather than just outputs/)
    
    Also some minor reformatting for readability

--- analysis/dataset_definition/dataset_definition_prevax.py
+++ analysis/dataset_definition/dataset_definition_prevax.py
@@ -6,21 +6,21 @@ from datetime import date
 
 # extract index dates for prevax cohort from index_dates.csv
 
-@table_from_file("output/index_dates.csv.gz")
+@table_from_file("output/dataset_definition/index_dates.csv.gz")
 
 class index_dates(PatientFrame):
-    index_prevax = Series(date)
+    index_prevax        = Series(date)
     end_prevax_exposure = Series(date)
-    end_prevax_outcome = Series(date)
+    end_prevax_outcome  = Series(date)
 
-index_date = index_dates.index_prevax
+index_date        = index_dates.index_prevax
 end_date_exposure = index_dates.end_prevax_exposure
-end_date_outcome = index_dates.end_prevax_outcome
+end_date_outcome  = index_dates.end_prevax_outcome
 
 # Create dataset
 
 dataset = generate_dataset(index_date, end_date_exposure, end_date_outcome)
 
-dataset.index_date = index_date
+dataset.index_date        = index_date
 dataset.end_date_exposure = end_date_exposure
-dataset.end_date_outcome = end_date_outcome
+dataset.end_date_outcome  = end_date_outcome
