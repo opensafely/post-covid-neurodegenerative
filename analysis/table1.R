4a8fab21547f201ac7fbe7d7661fa749c08d1176
Harry T Mason <harry.mason@outlook.com>
2025-02-25 10:05:48 +0000

    Ignore Mental Health reference files

--- analysis/table1.R
+++ analysis/table1.R
@@ -2,7 +2,7 @@
 print("Load libraries")
 
 library(magrittr)
-library('here')
+library(here)
 
 # Specify redaction threshold --------------------------------------------------
 print("Specify redaction threshold")
@@ -29,7 +29,45 @@ if (length(args) == 0) {
 # Load data --------------------------------------------------------------------
 print("Load data")
 
-df <- readr::read_rds(paste0("output/input_", cohort, ".rds"))# "_stage1.rds"))
+df <- readr::read_rds(paste0("output/dataset_clean/input_", cohort, ".rds"))# "_stage1.rds"))
+
+############################### MEDIAN_IQR_AGE #################################
+
+if (!file.exists(here::here(paste0(table1_dir, "median_iqr_age.csv")))) {
+
+  # Create empty output --------------------------------------------------------
+  print('Create empty output')
+
+  df_iqr <- data.frame(cohort = character(),
+               median_iqr_age = character())
+
+} else {
+
+  df_iqr <- read.csv(paste0(table1_dir, "median_iqr_age.csv"), header = TRUE)
+  df_iqr <- unique(df_iqr) # ensure it only has unique rows
+
+}
+
+# Calculate median (IQR) age -------------------------------------------------
+print("Calculate median (IQR) age")
+
+# if the row doesn't exist, create it. Otherwise, overwrite it
+if (sum(df_iqr["cohort"] == cohort) == 0) {
+  # Pastes: "Mean Age (LQ Age - UQ Age)" as a string for each cohort
+  df_iqr[nrow(df_iqr)+1, ] <- c(cohort, paste0(quantile(df$cov_num_age)[3], " (", quantile(df$cov_num_age)[2], "-", quantile(df$cov_num_age)[4], ")"))
+} else {
+  df_iqr[df_iqr$cohort ==cohort, ] <- c(cohort, paste0(quantile(df$cov_num_age)[3], " (", quantile(df$cov_num_age)[2], "-", quantile(df$cov_num_age)[4], ")"))
+}
+
+# Save median (IQR) age --------------------------------------------------------
+print("Save median (IQR) age")
+
+df_iqr <- df_iqr[order(df_iqr$cohort), ]  # sort table into order
+write.csv(df_iqr, paste0(table1_dir, "median_iqr_age.csv"), row.names = FALSE) # save
+
+################################## TABLE 1 #####################################
+
+print("Table 1 processing")
 
 # Remove people with history of COVID-19 ---------------------------------------
 print("Remove people with history of COVID-19")
@@ -60,7 +98,8 @@ print("Define age groups")
 #                                 & df$cov_num_consrate2019 <= 5, "1-5", df$cov_num_consrate2019)
 # df$cov_cat_consrate2019 <- ifelse(df$cov_num_consrate2019 >= 6,  "6+", df$cov_num_consrate2019)
 
-df$cov_cat_age_group <- numerical_to_categorical(df$cov_num_age,c(18,30,40,50,60,70,80,90),FALSE,FALSE,FALSE) # See utility.R
+# df$cov_cat_age_group <- numerical_to_categorical(df$cov_num_age,c(18,30,40,50,60,70,80,90),FALSE,FALSE,FALSE) # See utility.R
+df$cov_cat_age_group <- numerical_to_categorical(df$cov_num_age,c(18,40,65,85),FALSE,FALSE,FALSE) # See utility.R
 df$cov_cat_age_group <- ifelse(df$cov_cat_age_group == "<=17", "", df$cov_cat_age_group) # for consistency in blanking out underage
 
 df$cov_cat_consrate2019 <- numerical_to_categorical(df$cov_num_consrate2019,c(1,6),TRUE,FALSE,FALSE, FALSE)
@@ -80,13 +119,20 @@ print("Filter data")
 
 df <- df[, c("patient_id",
              "exposed",
-             colnames(df)[grepl("_cat_", colnames(df))]
-            #  "cov_bin_carehome"
-            #  colnames(df)[grepl("cov_bin_", colnames(df))
-            )]   # previously cov_cat_region
+             colnames(df)[grepl("cov_cat_", colnames(df))],
+             colnames(df)[grepl("strat_cat_", colnames(df))],
+             colnames(df)[grepl("cov_bin_", colnames(df))]
+            )]
 
 
 df$All <- "All"
+
+# Filter binary data
+
+for (colname in colnames(df)[grepl("cov_bin_", colnames(df))]) {
+  # df[[colname]] <- as.integer(df[[colname]]) # If they want as "0" and "1"
+  df[[colname]] <- sapply(df[[colname]], as.character)
+}
 
 # Aggregate data ---------------------------------------------------------------
 print("Aggregate data")
@@ -105,14 +151,12 @@ df <- aggregate(cbind(total, exposed) ~ characteristic + subcharacteristic,
 # Tidy care home characteristic ------------------------------------------------
 print("Remove extraneous information")
 
+df <- df[!(df$characteristic      == "cov_bin_carehome" &
+           df$subcharacteristic   == "FALSE"), ]
 
-# ADD BACK IN ONCE I FIGURE OUT BINARY VARIABLES
-# df <- df[!(df$characteristic      == "cov_bin_carehome_status" &
-#            df$subcharacteristic   == "FALSE"), ]
-
-# df$subcharacteristic <- ifelse(df$characteristic == "cov_bin_carehome_status",
-#                                "Care home resident",
-#                                df$subcharacteristic)
+df$subcharacteristic <- ifelse(df$characteristic == "cov_bin_carehome",
+                               "Care home resident",
+                               df$subcharacteristic)
 
 df <- df[df$subcharacteristic != FALSE, ] # From extendedtable.R
 df$subcharacteristic <- ifelse(df$subcharacteristic == "", # or == "unknown"??
@@ -125,39 +169,50 @@ print("Sort characteristics")
 df <- df[order(df$subcharacteristic, decreasing = TRUE), ] # Basic sort from extendedtable, might need to clean up
 df <- df[order(df$characteristic), ]
 
+# Try loading in plot_labels.csv
+
+df_lab <- read.csv("lib/plot_labels.csv", header = TRUE)
+
+# df_lab <- df_lab[, c(colnames(df)[grepl("cov_cat_", colnames(df))],
+#                      colnames(df)[grepl("strat_cat_", colnames(df))],
+#                      colnames(df)[grepl("cov_bin_", colnames(df))]
+#                    )]
+
+# setdiff(colnames(df), c("patient_id", "exposed"))
+
 # df$characteristic <- factor(df$characteristic,
 #                             levels = c("All",
 #                                        "cov_cat_sex",
 #                                        "cov_cat_age_group",
 #                                        "cov_cat_ethnicity",
-#                                        "cov_cat_deprivation",
-#                                        "cov_cat_smoking_status",
-#                                        "cov_cat_region",
+#                                        "cov_cat_imd",
+#                                        "cov_cat_smoking",
+#                                        "strat_cat_region",
 #                                        "cov_bin_carehome_status"),
 #                             labels = c("All",
 #                                        "Sex",
 #                                        "Age, years",
 #                                        "Ethnicity",
-#                                        "Index of multuple deprivation quintile",
+#                                        "Index of multiple deprivation quintile",
 #                                        "Smoking status",
 #                                        "Region",
 #                                        "Care home resident"))
 
-# # Sort subcharacteristics ------------------------------------------------------
-# print("Sort subcharacteristics")
+df$characteristic <- factor(df$characteristic,
+                            levels = df_lab$term,
+                            labels = df_lab$label)
+
+# Sort subcharacteristics ------------------------------------------------------
+print("Sort subcharacteristics")
 
 # df$subcharacteristic <- factor(df$subcharacteristic,
 #                                levels = c("All",
 #                                           "Female",            # Sex
 #                                           "Male",
-#                                           "18-29",             # Age
-#                                           "30-39",
-#                                           "40-49",
-#                                           "50-59",
-#                                           "60-69",
-#                                           "70-79",
-#                                           "80-89",
-#                                           "90+",
+#                                           "18-39",             # Age
+#                                           "40-64",
+#                                           "65-84",
+#                                           "85-110",
 #                                           "White",            # Ethnicity
 #                                           "Mixed",
 #                                           "South Asian",
@@ -186,14 +241,10 @@ df <- df[order(df$characteristic), ]
 #                                labels = c("All",
 #                                           "Female",             # Sex
 #                                           "Male",
-#                                           "18-29",              # Age
-#                                           "30-39",
-#                                           "40-49",
-#                                           "50-59",
-#                                           "60-69",
-#                                           "70-79",
-#                                           "80-89",
-#                                           "90+",
+#                                           "18-39",             # Age
+#                                           "40-64",
+#                                           "65-84",
+#                                           "85-110",
 #                                           "White",              # Ethnicity
 #                                           "Mixed",
 #                                           "South Asian",
@@ -226,21 +277,19 @@ print("Sort data")
 
 df <- df[order(df$characteristic, df$subcharacteristic), ]
 
-# Define output folder ---------------------------------------------------------
-print("checking output folder")
+# Define table1 output folder ---------------------------------------------------------
+print("Creating output/table1 output folder")
 
 # setting up the sub directory
-sub_dir <- "output/table1/"
- 
+table1_dir <- "output/table1/"
+
 # check if sub directory exists, create if not
-if (!file.exists(sub_dir)) {
-  dir.create(file.path(sub_dir))
-}
+fs::dir_create(here::here(table1_dir))
 
 # Save Table 1 -----------------------------------------------------------------
 print("Save Table 1")
 
-write.csv(df, paste0(sub_dir, "table1_", cohort, ".csv"), row.names = FALSE)
+write.csv(df, paste0(table1_dir, "table1_", cohort, ".csv"), row.names = FALSE)
 
 # Perform redaction ------------------------------------------------------------
 print("Perform redaction")
@@ -284,4 +333,41 @@ colnames(df) <- c("Characteristic",
 # Save Table 1 -----------------------------------------------------------------
 print("Save rounded Table 1")
 
-write.csv(df, paste0(sub_dir, "table1_", cohort, "_midpoint6.csv"), row.names = FALSE)
+write.csv(df, paste0(table1_dir, "table1_", cohort, "_midpoint6.csv"), row.names = FALSE)
+
+
+
+# Median IQR age
+
+if (!file.exists(here::here(paste0(table1_dir, "median_iqr_age.csv")))) {
+
+  # Create empty output ----------------------------------------------------------
+  print('Create empty output')
+
+  df_iqr <- data.frame(cohort = character(),
+                  median_iqr_age = character())
+
+  # Loop over cohorts ------------------------------------------------------------
+  print("Loop over cohorts")
+
+  for (cohort in c("prevax", "vax", "unvax")) { #c("prevax_extf", "vax", "unvax_extf")
+
+    # Load data ------------------------------------------------------------------
+    print(paste0("Load data for cohort: ", cohort))
+
+    tmp <- readr::read_rds(paste0("output/dataset_clean/input_", cohort, ".rds")) #"_stage1.rds"
+
+    # Calculate median (IQR) age -------------------------------------------------
+    print("Calculate median (IQR) age")
+
+    # Pastes: "Mean Age (LQ Age - UQ Age)" as a string for each cohort
+    df_iqr[nrow(df_iqr)+1, ] <- c(cohort, paste0(quantile(tmp$cov_num_age)[3], " (", quantile(tmp$cov_num_age)[2], "-", quantile(tmp$cov_num_age)[4], ")"))
+    
+  }
+
+  # Save median (IQR) age --------------------------------------------------------
+  print('Save median (IQR) age')
+
+  write.csv(df_iqr, paste0(table1_dir, "median_iqr_age.csv"), row.names = FALSE)
+
+}
