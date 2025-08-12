e5f246c3c8b99353fe0314c5ffcb97ac24fa8938
Harry T Mason <harry.mason@outlook.com>
2025-08-12 16:02:00 +0100

    Include CTV3 code

--- analysis/dataset_definition_rsd/variable_rsd_helper_functions.py
+++ analysis/dataset_definition_rsd/variable_rsd_helper_functions.py
@@ -31,7 +31,15 @@ def first_matching_event_apc_between(codelist, start_date, end_date, only_prim_d
     else:
         query = query.where(apcs.all_diagnoses.contains_any_of(codelist))
     return query.sort_by(apcs.admission_date).first_for_patient()
-
+  
+def first_matching_event_clinical_ctv3_between(codelist, start_date, end_date, where=True):
+    return(
+        clinical_events.where(where)
+        .where(clinical_events.ctv3_code.is_in(codelist))
+        .where(clinical_events.date.is_on_or_between(start_date, end_date))
+        .sort_by(clinical_events.date)
+        .first_for_patient()
+    )
 
 def matching_death_between(codelist, start_date, end_date, where=True):
     return(
