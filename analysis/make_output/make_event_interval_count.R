320e50d90d2c10750cf775b924007ca093dee9a4
Harry T Mason <harry.mason@outlook.com>
2026-03-19 15:55:24 +0000

    Updates to autofileter days, include days_pre, minimise columns loaded, and add in custom combination columns

--- analysis/make_output/make_event_interval_count.R
+++ analysis/make_output/make_event_interval_count.R
@@ -1,9 +1,9 @@
 library(dplyr)
 library(tidyr)
+library(readr)
+library(stringr)
 # Load data --------------------------------------------------------------------
 print("Load data")
-
-print("Load model output")
 
 # List all CSV files matching the pattern
 file_list <- list.files(
@@ -14,20 +14,32 @@ file_list <- list.files(
 
 # Read and combine all CSV files into one data frame
 df <- file_list %>%
-  lapply(read_csv, show_col_types = FALSE) %>%
+  lapply(
+    read_csv,
+    show_col_types = FALSE,
+    col_select = c(
+      # "name",
+      "cohort",
+      "analysis",
+      "outcome",
+      "term",
+      "model",
+      "source",
+      "hr",
+      "N_events_midpoint6"
+    )
+  ) %>%
   bind_rows()
 
-# Find analyses with <12 midpoint6 events at any timepoint
-low_event_list <- unique(na.omit(df[df$N_events_midpoint6 < 12, ]$name)) # find list
-
-df_filtered <- df[!(df$name %in% low_event_list), ] # apply reduction
-
-# Save list of removed files
-sink('output/make_output/removed_models_low_events.txt')
-print(low_event_list)
-sink() #close external connection to file
-
-df <- df[!is.na(df$hr), ]
+# # Find analyses with <12 midpoint6 events at any timepoint
+# low_event_list <- unique(na.omit(df[df$N_events_midpoint6 < 12, ]$name)) # find list
+#
+# df_filtered <- df[!(df$name %in% low_event_list), ] # apply reduction
+#
+# # Save list of removed files
+# sink('output/make_output/removed_models_low_events.txt')
+# print(low_event_list)
+# sink() #close external connection to file
 
 # Filter data ------------------------------------------------------------------
 print("Filter data")
@@ -39,29 +51,22 @@ df <- df[
     "cohort",
     "analysis",
     "outcome",
-    "outcome_time_median",
     "term",
-    "hr",
-    "conf_low",
-    "conf_high",
     "source",
-    "N_total_midpoint6",
-    "N_exposed_midpoint6",
     "N_events_midpoint6"
   )
 ]
 
+days_list <- unique(df$term[grepl("days", df$term)])
+
+sorted_days <- days_list[order(sapply(days_list, function(x) {
+  if (x == "days_pre") return(-1) # Ensure 'pre' is always first
+  as.numeric(str_extract(x, "\\d+")) # Extract the first number
+}))]
+
 df$term <- factor(
   df$term,
-  levels = c(
-    "days0_28",
-    "days28_183",
-    "days183_365",
-    "days365_730",
-    "days730_1095",
-    "days1095_1460",
-    "days1460_1979"
-  ),
+  levels = sorted_days,
   ordered = TRUE
 )
 
@@ -83,17 +88,37 @@ df_wide <- df %>%
     all_of(levels(df$term))
   )
 
-df_wide["6month_Sum"] <- df_wide["days0_28"] + df_wide["days28_183"]
-df_wide["Year1_Sum"] <- df_wide["days0_28"] +
+df_wide <- df_wide[!is.na(df_wide$analysis), ]
+
+# From day1 sums
+df_wide["days0_183_derived"] <- df_wide["days0_28"] + df_wide["days28_183"]
+df_wide["days0_365_derived"] <- df_wide["days0_28"] +
   df_wide["days28_183"] +
   df_wide["days183_365"]
-df_wide["Total_Sum"] <- df_wide["days0_28"] +
-  df_wide["days28_183"] +
-  df_wide["days183_365"] +
-  df_wide["days365_730"] +
+df_wide["Total"] <- rowSums(
+  df_wide[sorted_days[2:length(sorted_days)]],
+  na.rm = TRUE
+)
+
+# Later sums
+df_wide["days0_28A"] <- df_wide["days0_28"]
+df_wide["days28_365A_derived"] <- df_wide["days28_183"] + df_wide["days183_365"]
+df_wide["days365_1460A_derived"] <- df_wide["days365_730"] +
   df_wide["days730_1095"] +
-  df_wide["days1095_1460"] +
-  df_wide["days1460_1979"]
+  df_wide["days1095_1460"]
+df_wide["days0_28B"] <- df_wide["days0_28"]
+df_wide["days28_730B_derived"] <- df_wide["days28_183"] +
+  df_wide["days183_365"] +
+  df_wide["days365_730"]
+df_wide["days730_1460B_derived"] <- df_wide["days730_1095"] +
+  df_wide["days1095_1460"]
+df_wide["days0_28C"] <- df_wide["days0_28"]
+df_wide["days28_183C"] <- df_wide["days28_183"]
+df_wide["days183_730C_derived"] <- df_wide["days183_365"] +
+  df_wide["days365_730"]
+df_wide["days730_1460C_derived"] <- df_wide["days730_1095"] +
+  df_wide["days1095_1460"]
+
 
 readr::write_csv(
   df_wide,
